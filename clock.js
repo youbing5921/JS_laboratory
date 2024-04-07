30a170ad62556fd42a412bfa409ed02fa6e10f04
youbing5921 <ahdtlf2002@sookmyung.ac.kr>
2024-04-07 17:15:35 +0900

    images

--- clock.js
+++ clock.js
@@ -1,13 +1,13 @@
-const clock = document.querySelector("h3#clock");
+const clock = document.querySelector("#clock h3");
 function getClock() {
     const date = new Date();
-    const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const day = ["", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
     const dayOfTheWeek = day[date.getDate()];
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
 
-    clock.innerText = `현재 시각 : ${date.getFullYear()}.${date.getMonth()}.${date.getDay()}(${dayOfTheWeek}) ${hours}:${minutes}:${seconds}`;
+    clock.innerText = `현재 시각 : ${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}(${dayOfTheWeek}) ${hours}:${minutes}:${seconds}`;
 }
 getClock();
 setInterval(getClock, 1000);
